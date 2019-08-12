67fc753f79ad7e3c577a0763277b2a4ab35219e4
itarvin <itarvin@163.com>
2019-08-12 14:57:32 +0800

    二七文章网

---
+++ twoarticle/main.py
@@ -0,0 +1,3 @@
+from scrapy import cmdline
+cmdline.execute('scrapy crawl article'.split())
+# /html/body/div[1]/div/div[1]/div/article/p[4]/img
---
+++ twoarticle/twoarticle/spiders/article.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from twoarticle.items import TwoarticleItem
+
+class ArticleSpider(CrawlSpider):
+    name = 'article'
+    allowed_domains = ['www.027it.cn']
+    start_urls = ['http://www.027it.cn/list-%d-0.html' % i for i in range(1,216)]
+    # start_urls = ['http://www.027it.cn/list-215-0.html']
+    pagelink = LinkExtractor(allow=r'http://www.027it.cn/list-\d+-\d+.html')
+    contentlink = LinkExtractor(allow=r'http://www.027it.cn/article-\d+-\d+-\d+.html')
+    rules = (
+        Rule(contentlink, callback = 'parse_item',follow=True),
+        Rule(pagelink),
+    )
+
+    def parse_item(self, response):
+        item = TwoarticleItem()
+        item['title'] = response.xpath('//*[@id="title"]/h1/a/text()').extract()[-1]
+        # 添加时间
+        item['addtime'] = response.xpath('//*[@id="title"]/text()').extract()[-2].split()[1]
+        item['cate'] = response.xpath('//*[@id="place"]/a[3]/text()').extract()[-1]
+        # 内容
+        item['content'] = response.xpath('//*[@id="contents"]').extract()
+        # 链接
+        item['url'] = response.url
+        #
+        yield item
